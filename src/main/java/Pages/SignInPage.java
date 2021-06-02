fd9a9a03474180c6265b0658fafead5350f5bc63
memrebir <m.emrebir@gmail.com>
2021-06-02 16:19:19 +0300

    find element type changed

--- src/main/java/Pages/SignInPage.java
+++ src/main/java/Pages/SignInPage.java
@@ -7,18 +7,17 @@ public class SignInPage {
 
     By userName = By.id("EmailLogin");
     By password = By.id("Password");
-    By btnLogin = By.cssSelector("#userLogin > div:nth-child(6) > button");
-    By btnForgotPassword = By.cssSelector("#userLogin > div:nth-child(6) > a");
+    By btnLogin = By.className("js-login-button");
+    By btnForgotPassword = By.className("js-forgot-password");
     By textForgotPassword = By.id("Mail");
-    By btnForgetPswSend = By.cssSelector("body > main > div > div:nth-child(1) > div >" +
-            " form.form-password-recovery.js-password-recovery.is-hidden > div:nth-child(3) > button");
-    By btnSignUp = By.cssSelector("body > main > div > div:nth-child(2) > div > a.membership-advantages__button--member-login.btn.btn-warning.btn-lg");
-    By btnSignInWithFacebook = By.cssSelector("body > main > div > div:nth-child(2) > div > a.login__facebook.btn.btn-info.btn-xlg.full-width");
+    By btnForgetPswSend = By.className("js-password-recovery-button");
+    By btnSignUp = By.className("btn-warning");
+    By btnSignInWithFacebook = By.className("login__social-text");
 
-    public static By alertModal = By.cssSelector("#modalBox > div > div > div.modal-body");
+    public static By alertModal = By.className("modal-body");
     public static By requiredFieldEmail = By.id("EmailLogin-error");
     public static By requiredFieldPassword = By.id("Password-error");
-    public static By forgetSuccessIcon = By.cssSelector("body > main > div > div:nth-child(1) > div > div > span");
+    public static By forgetSuccessIcon = By.className("password-recovery-result__icon");
 
     private final WebDriver  driver;
 
--- src/main/java/Pages/SignUpPage.java
+++ src/main/java/Pages/SignUpPage.java
@@ -4,5 +4,5 @@ import org.openqa.selenium.By;
 
 public class SignUpPage {
 
-    public static By registerTextElement = By.cssSelector("body > main > div > div:nth-child(1) > div > h2");
+    public static By registerTextElement = By.className("login__heading");
 }
